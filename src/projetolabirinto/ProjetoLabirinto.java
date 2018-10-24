ec06ce94acbc617043baad45f04cdbbbe5986107
Vinicius Pazinatto <vinipazinatto@yahoo.com.br>
2018-10-23 21:35:55 -0300

    first

--- src/projetolabirinto/ProjetoLabirinto.java
+++ src/projetolabirinto/ProjetoLabirinto.java
@@ -5,6 +5,9 @@
  */
 package projetolabirinto;
 
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author vinip
@@ -14,8 +17,16 @@ public class ProjetoLabirinto {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        File file = new File("F:\\ProjetoLabirinto\\arquivos-texto\\labirinto1.txt"); 
+  
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        
+        String labirinto;
+        while((labirinto = br.readLine()) != null)
+            System.out.println(labirinto);
+
     }
     
 }
