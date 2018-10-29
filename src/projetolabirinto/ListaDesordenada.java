30b465d6e4ef06eb66075dc6dd7d7a9b05584096
Vinicius Pazinatto <vinipazinatto@yahoo.com.br>
2018-10-29 20:42:14 -0300

    src

--- src/projetolabirinto/ListaDesordenada.java
+++ src/projetolabirinto/ListaDesordenada.java
@@ -2,8 +2,67 @@ package projetolabirinto;
 
 public class ListaDesordenada<X> {
     
+    private class No
+    {
+        private X  info;
+        private No prox;
+
+        public No (X i, No p)
+        {
+            this.info = i;
+            this.prox = p;
+        }
+
+        public X getInfo ()
+        {
+            return this.info;
+        }
+
+        public No getProx ()
+        {
+            return this.prox;
+        }
+
+        public void setInfo (X i)
+        {
+            this.info = i;
+        }
+
+        public void setProx (No p)
+        {
+            this.prox = p;
+        }
+    }
+    
+    private No prim;
+    private No ulti;
+    
     public ListaDesordenada() {
+        this.prim = null;
+        this.ulti = null;
+    }
+    
+    public boolean vazia() 
+    {
+        if (this.prim == null)
+            return true;
+        else
+            return false;
+    }
+    
+    public void insiraNoInicio(X i) {
         
     }
     
+    public void removeInicio() {
+        
+    }
+    
+    public void removeFinal() {
+    
+    }
+    
+    public void insiraNoFinal() {
+    
+    }
 }
