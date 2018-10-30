2c5e402b08d58e4eb1ca21f694626921a5a8a5d9
dancmoura <44419299+dancmoura@users.noreply.github.com>
2018-10-29 22:19:47 -0300

    src

--- src/projetolabirinto/Programa.java
+++ src/projetolabirinto/Programa.java
@@ -11,16 +11,11 @@ public class Programa {
         // TODO code application logic here
         
         Labirinto labirinto = new Labirinto();
+        Pilha<Coordenada> caminho = new Pilha<Coordenada> ();
+        Pilha<Fila<Coordenada>> possibilidades = new Pilha<Fila<Coordenada>> ();
+        Fila<Coordenada> fila = new Fila<Coordenada> ();
         
-        System.out.println(labirinto.procuraEntradaESaida());
-        
-        /*
-        Pilha<Coordenada> caminho = new Pilha<Coordenada> (40);
-        Pilha<Fila<Coordenada>> possibilidades = new Pilha<Fila<Coordenada>> (40);
-        Fila<Coordenada> fila = new Fila<Coordenada> (3);
-        
-        caminho.insereUmItem(new Coordenada(1, 1));
+        caminho.guardeUmItem(new Coordenada(1, 1));
         System.out.println(caminho);
-        */
     }
 }
