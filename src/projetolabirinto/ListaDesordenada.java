ff24f9c6f4e030940ec3ed7a6029e4e9cbd21778
Vinicius Pazinatto <vinipazinatto@yahoo.com.br>
2018-10-29 21:14:09 -0300

    i

--- src/projetolabirinto/ListaDesordenada.java
+++ src/projetolabirinto/ListaDesordenada.java
@@ -1,5 +1,7 @@
 package projetolabirinto;
 
+import java.lang.reflect.*;
+
 public class ListaDesordenada<X> {
     
     private class No
@@ -42,6 +44,24 @@ public class ListaDesordenada<X> {
         this.ulti = null;
     }
     
+    private X meuCloneDeX (X x)
+    {
+        X ret=null;
+
+        try
+        {
+            Class<?> classe = x.getClass();
+            Class<?>[] tipoParametroFormal = null; // null pq clone tem 0 parametros
+            Method metodo = classe.getMethod ("clone", tipoParametroFormal);
+            Object[] parametroReal = null; // null pq clone tem 0 parametros
+            ret = (X)metodo.invoke (x, parametroReal);
+        }
+        catch (Exception erro)
+        {}
+
+        return ret;
+    }
+    
     public boolean vazia() 
     {
         if (this.prim == null)
@@ -50,12 +70,21 @@ public class ListaDesordenada<X> {
             return false;
     }
     
-    public void insiraNoInicio(X i) {
+    public void insiraNoInicio(X i) throws Exception {
+        if (i == null)
+            throw new Exception("Erro ao inserir");
         
+        X info;
+        if (i instanceof Cloneable)
+            info = meuCloneDeX (i);
+        else
+            info = i;
+        
+        this.prim = new No(info, null);
     }
     
     public void insiraNoFinal(X i) {
-    
+        
     }
     
     public void removePrimeiro() {
