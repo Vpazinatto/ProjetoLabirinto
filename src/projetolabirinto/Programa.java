7cc2b96c7778952597ff3f1460e41ccc1a99709b
Vpazinatto <40928534+Vpazinatto@users.noreply.github.com>
2018-11-13 16:40:19 -0200

    FINISH

--- src/projetolabirinto/Programa.java
+++ src/projetolabirinto/Programa.java
@@ -6,8 +6,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
- * @author Dan
+ *@author Vinicius Pazinatto
+ * @author Daniel Carvalho de Moura
  */
 public class Programa {
 
@@ -20,52 +20,52 @@ public class Programa {
     public static void main(String[] args) throws IOException, Exception {
         try
         {
-        Labirinto labirinto = new Labirinto();
-        String nome = "";
-        Pilha<Coordenada> caminho = new Pilha<Coordenada> ();
-        Pilha<Fila<Coordenada>> possibilidades = new Pilha<Fila<Coordenada>> ();
-        
-        Scanner s = new Scanner(System.in);
-        File arquivos[];
-        File arquivo = new File("arquivos-texto");
-        arquivos = arquivo.listFiles();
-        
-        for(int i = 0; i < arquivos.length; i++){
-            System.out.println(arquivos[i].getName());
-        }
-        
-        System.out.println();
-        System.out.print("Escolha um dos labirintos acima: ");
-        String file = s.nextLine();
-        
-        for(int i = 0; i < arquivos.length; i++){
-            if (arquivos[i].getName().equals(file)) {
-                file = arquivos[i].getAbsolutePath();
-                nome = arquivos[i].getName();
+            Labirinto labirinto = new Labirinto();
+            String nome = "";
+            Pilha<Coordenada> caminho = new Pilha<Coordenada> ();
+            Pilha<Fila<Coordenada>> possibilidades = new Pilha<Fila<Coordenada>> ();
+
+            Scanner s = new Scanner(System.in);
+            File arquivos[];
+            File arquivo = new File("arquivos-texto");
+            arquivos = arquivo.listFiles();
+
+            for(int i = 0; i < arquivos.length; i++){
+                System.out.println(arquivos[i].getName());
             }
-        }
-            
-        System.out.println();
-        
-        labirinto.montaLabirinto(file);
-        labirinto.procuraEntradaESaida();
-        labirinto.preencheCaminho();
-        
-        PrintWriter saida =
-            new PrintWriter (
-            new FileWriter (
-            "Resolvido_" + nome));
-        
-        for (int l = 0; l < labirinto.getLinhasQtd(); l++) {
-            for (int c = 0; c < labirinto.getColunasQtd(); c++) {
-                saida.print(labirinto.getLabirinto()[l][c]);
+
+            System.out.println();
+            System.out.print("Escolha um dos labirintos acima: ");
+            String file = s.nextLine();
+
+            for(int i = 0; i < arquivos.length; i++){
+                if (arquivos[i].getName().equals(file)) {
+                    file = arquivos[i].getAbsolutePath();
+                    nome = arquivos[i].getName();
+                }
             }
-            saida.println();
-        }
-        
-        saida.close();
-        
-        System.out.println("Saída encontrada, o labirinto resolvido está na pasta!");
+
+            System.out.println();
+
+            labirinto.montaLabirinto(file);
+            labirinto.procuraEntradaESaida();
+            labirinto.preencheCaminho();
+
+            PrintWriter saida =
+                new PrintWriter (
+                new FileWriter (
+                "Resolvido_" + nome));
+
+            for (int l = 0; l < labirinto.getLinhasQtd(); l++) {
+                for (int c = 0; c < labirinto.getColunasQtd(); c++) {
+                    saida.print(labirinto.getLabirinto()[l][c]);
+                }
+                saida.println();
+            }
+
+            saida.close();
+
+            System.out.println("Saída encontrada, o labirinto resolvido está na pasta!");
         }
         catch (Exception e)
         {}
